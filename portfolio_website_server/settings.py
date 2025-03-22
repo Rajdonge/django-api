e3fb16d487fe407bef5d2c6e4f8685a7c9298fb0
Bibek Dhimal <bbekdml2020@gmail.com>
2025-03-22 14:09:53 +0545

    Some changes

--- portfolio_website_server/settings.py
+++ portfolio_website_server/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-amuks@_2f*mkp89*7%$$a6sp)(snp1zcmx_(uu2mp72hm*6wt6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
